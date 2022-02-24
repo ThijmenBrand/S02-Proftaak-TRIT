8a24c8ce6dfb644e3b3041dca541e7a278163675
LukasJansen100 <93530655+LukasJansen100@users.noreply.github.com>
2022-02-24 15:37:54 +0100

    Created the tab

---
+++ kdp-frontend/src/views/Tribe-Information-Page/Components/Profiletag.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div class="image">
+
+        </div>
+        <p class="rockstar">{{name}}</p>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name:"Profiletag",
+    props:{
+        name: String,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.image{
+    height: 60px;
+    width: 60px;
+    background-color: white;
+    border-radius: 50%;
+}
+.rockstar {
+    color: white;
+}
+</style>
---
+++ kdp-frontend/src/views/Tribe-Information-Page/InformationPage.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="InformationPageMain">
+        <div class="Overview"> 
+            <h3 class="TribeTitle">Vue.js</h3>
+            <div class="profile-container" >
+                <profiletag v-for="profile in profiles" :key="profile" :name="profile"/>
+            </div>
+        </div>
+            
+        <div class="Articles">
+            <p>Articles</p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Profiletag from "./Components/Profiletag.vue";
+export default {
+    components:{
+        Profiletag
+    },
+    setup(){
+        const profiles = ["Profile1", "Profile2", "Profile3"];
+        return {profiles};
+    }
+}
+
+</script>
+
+<style scoped>
+p {
+    color: white;
+    margin:0;
+}
+.Overview{
+    background-color:yellow;
+    width: auto;
+    height: 300px;
+    margin: 0%;
+    padding: 0%;
+
+}
+.Articles{
+    background-color: black;
+    height:1000px;
+    margin: 0%;
+    padding: 0%;
+}
+.TribeTitle{
+    padding: 20px;
+    padding-left: 40px;
+}
+.profile-container{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+
+}
+</style>
