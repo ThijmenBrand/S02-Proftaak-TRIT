9f107f8c26538c6f2e7cade0433ac73bc2742b4d
Laar,Erik E.J.J. Van de <erik.vandelaar@student.fontys.nl>
2022-02-24 16:38:43 +0100

    Add basic yellow header with logo - still needs hamburger

--- kdp-frontend/src/components/navigation/BaseNavigation.vue
+++ kdp-frontend/src/components/navigation/BaseNavigation.vue
@@ -1,9 +1,33 @@
 <template>
-  <h1>Nav</h1>
+  <div class="container">
+    <div class="logo">
+      <img src="@/assets/logo.png" alt="RockstarsIT Logo" />
+    </div>
+  </div>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+export default {
+  components: {
+
+  }
+};
 </script>
 
-<style></style>
+<style scoped>
+.container {
+  background-color: #ffe000;
+  width: 100%;
+  height: 12em;
+}
+
+.logo {
+  margin: 0;
+  padding: 0;
+}
+
+.logo img {
+  height: calc(12em - (20px / 2));
+  filter: drop-shadow(2px 2px 2px gray)
+}
+</style>
