0900a0e17ce2098328ce32ee646025dcb0949e73
Erik Van de Laar <ejjvandelaar@gmail.com>
2022-02-25 12:49:46 +0100

    Add hamburger menu with animation - span click box still needs expanding

--- kdp-frontend/src/components/navigation/BaseNavigation.vue
+++ kdp-frontend/src/components/navigation/BaseNavigation.vue
@@ -4,13 +4,25 @@
       <img src="@/assets/logo.png" alt="RockstarsIT Logo" />
     </div>
   </div>
+  <div class="hamburger-menu">
+    <input id="menu-toggle" type="checkbox"/>
+    <label class="menu-btn" for="menu-toggle">
+      <span></span>
+    </label>
+    
+    <ul class="menu-box">
+      <li><a class="menu-item" href="#">HOME</a></li>
+      <li><a class="menu-item" href="#">OUR TEAM</a></li>
+      <li><a class="menu-item" href="#">OUR ASPIRATIONS</a></li>
+      <li><a class="menu-item" href="#">OUR VISION</a></li>
+      <li><a class="menu-item" href="#">YOUR POSSIBILITIES</a></li>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts">
 export default {
-  components: {
-
-  }
+  components: {}
 };
 </script>
 
@@ -28,6 +40,83 @@ export default {
 
 .logo img {
   height: calc(12em - (20px / 2));
-  filter: drop-shadow(2px 2px 2px gray)
+  filter: drop-shadow(2px 2px 2px gray);
 }
+  
+/* hamburger menu */
+#menu-toggle {
+  opacity: 0;
+}
+#menu-toggle:checked + .menu-btn > span {
+  transform: rotate(45deg);
+}
+#menu-toggle:checked + .menu-btn > span::before {
+  top: 0;
+  transform: rotate(0deg);
+  background-color: #ffffff;
+}
+#menu-toggle:checked + .menu-btn > span::after {
+  top: 0;
+  transform: rotate(90deg);
+  background-color: #ffffff;
+}
+#menu-toggle:checked ~ .menu-box {
+  right: 0 !important;
+}
+.menu-btn {
+  position: fixed;
+  top: 40px;
+  right: 40px;
+  width: 26px;
+  
+  cursor: pointer;
+  z-index: 1;
+}
+.menu-btn > span,
+.menu-btn > span::before,
+.menu-btn > span::after {
+  display: block;
+  position: absolute;
+  width: 100%;
+  height: 2px;
+  background-color: #616161;
+  transition-duration: .25s;
+}
+.menu-btn > span::before {
+  content: '';
+  top: -8px;
+}
+.menu-btn > span::after {
+  content: '';
+  top: 8px;
+}
+.menu-box {
+  display: block;
+  position: fixed;
+  top: 0;
+  right: -100%;
+  width: 300px;
+  height: 100%;
+  margin: 0;
+  padding: 80px 0;
+  list-style: none;
+  background-color: #232323;
+  box-shadow: 2px 2px 6px rgba(0, 0, 0, .4);
+  transition-duration: .25s;
+}
+.menu-item {
+  display: block;
+  padding: 12px 24px;
+  color: #ffffff;
+  font-family: 'Roboto', sans-serif;
+  font-size: 20px;
+  font-weight: 600;
+  text-decoration: none;
+  transition-duration: .25s;
+}
+.menu-item:hover {
+  transition: color 0.25s ease-in;
+  color: #ffe000;
+}
+
 </style>
