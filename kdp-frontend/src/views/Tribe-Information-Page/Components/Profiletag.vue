8cc62d45d9eddcd5b14039747575963f4a25c8aa
Jansen,Lukas L.J <lukas.jansen@student.fontys.nl>
2022-02-25 10:11:34 +0100

    Added ArticlePreview

--- kdp-frontend/src/views/Tribe-Information-Page/Components/Profiletag.vue
+++ kdp-frontend/src/views/Tribe-Information-Page/Components/Profiletag.vue
@@ -1,29 +1,27 @@
 <template>
-    <div>
-        <div class="image">
-
-        </div>
-        <p class="rockstar">{{name}}</p>
-    </div>
+  <div>
+    <div class="image"></div>
+    <p class="rockstar">{{ name }}</p>
+  </div>
 </template>
 
 <script lang="ts">
 export default {
-    name:"Profiletag",
-    props:{
-        name: String,
-    }
-}
+  name: "Profiletag",
+  props: {
+    name: String,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-.image{
-    height: 60px;
-    width: 60px;
-    background-color: white;
-    border-radius: 50%;
+.image {
+  height: 60px;
+  width: 60px;
+  background-color: white;
+  border-radius: 50%;
 }
 .rockstar {
-    color: white;
+  color: white;
 }
 </style>
