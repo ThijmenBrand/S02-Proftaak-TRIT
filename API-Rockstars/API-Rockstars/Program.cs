873a6b6def986d4bef41e931b3a730462ec78f4b
Reno Muijsenberg <81637@roc-teraa.nl>
2022-03-11 09:13:16 +0100

    ADd new packages

--- API-Rockstars/API-Rockstars/Program.cs
+++ API-Rockstars/API-Rockstars/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));  
 
