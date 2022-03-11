a3dcf37636706b18f5eec83f2122985319184b62
Reno Muijsenberg <81637@roc-teraa.nl>
2022-03-11 09:25:13 +0100

    Add rockstar controller

---
+++ API-Rockstars/API-Rockstars/Controllers/RockstarController.cs
@@ -0,0 +1,109 @@
+#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using API_Rockstars;
+using API_Rockstars.Models;
+
+namespace API_Rockstars.Controllers
+{
+    [Route("api/rockstar")]
+    [ApiController]
+    public class RockstarController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RockstarController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Rockstar
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Rockstar>>> GetRockstars()
+        {
+            return await _context.Rockstars.ToListAsync();
+        }
+
+        // GET: api/Rockstar/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Rockstar>> GetRockstar(Guid id)
+        {
+            var rockstar = await _context.Rockstars.FindAsync(id);
+
+            if (rockstar == null)
+            {
+                return NotFound();
+            }
+
+            return rockstar;
+        }
+
+        // PUT: api/Rockstar/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRockstar(Guid id, Rockstar rockstar)
+        {
+            if (id != rockstar.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(rockstar).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RockstarExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Rockstar
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Rockstar>> PostRockstar(Rockstar rockstar)
+        {
+            _context.Rockstars.Add(rockstar);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetRockstar", new { id = rockstar.Id }, rockstar);
+        }
+
+        // DELETE: api/Rockstar/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRockstar(Guid id)
+        {
+            var rockstar = await _context.Rockstars.FindAsync(id);
+            if (rockstar == null)
+            {
+                return NotFound();
+            }
+
+            _context.Rockstars.Remove(rockstar);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool RockstarExists(Guid id)
+        {
+            return _context.Rockstars.Any(e => e.Id == id);
+        }
+    }
+}
