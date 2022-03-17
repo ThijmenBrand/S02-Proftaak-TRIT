a482f437b00ee71451919750a4f7c150a25dfdb4
Laar,Erik E.J.J. Van de <erik.vandelaar@student.fontys.nl>
2022-03-17 14:34:12 +0100

    Added rockstar information page route to router

--- kdp-frontend/src/router/index.ts
+++ kdp-frontend/src/router/index.ts
@@ -3,6 +3,7 @@ import Home from '../views/Home.vue';
 import tribes from '@/views/tribes/Tribes.vue';
 import tribe from '@/views/tribes/Tribe-Information-Page/InformationPage.vue';
 import article from '@/views/tribes/article/Article.vue';
+import rockstar from '@/views/rockstars/Rockstar-Information-Page/InformationPage.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -25,6 +26,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/tribe/:tribe/article/:articleId',
     name: 'article',
     component: article,
+  },
+  {
+    path: '/rockstar/:rockstarId',
+    name: 'rockstar',
+    component: rockstar,
   },
 ];
 
