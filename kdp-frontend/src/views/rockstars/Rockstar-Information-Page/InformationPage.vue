0d2e8342e552164fdb0a41735ff8a79d5972cc87
Laar,Erik E.J.J. Van de <erik.vandelaar@student.fontys.nl>
2022-03-17 14:55:57 +0100

    Added GET to information page to view Rockstar information

--- kdp-frontend/src/views/rockstars/Rockstar-Information-Page/InformationPage.vue
+++ kdp-frontend/src/views/rockstars/Rockstar-Information-Page/InformationPage.vue
@@ -1,13 +1,40 @@
 ﻿<template>
-$END$
+  <h1>{{ rockstar.id }}</h1>
+  <h1>{{ rockstar.name }}</h1>
+  <h1>{{ rockstar.description }}</h1>
 </template>
 
-<script>
+<script lang="ts">
+import { useRoute } from 'vue-router';
+import { useStore } from 'vuex';
+import { RockstarShape } from "@/models/Rockstar";
+import { computed, onMounted } from "vue";
+import rockstar from "@/views/rockstars/store/rockstars";
+
 export default {
-name: "InformationPage"
-}
+  setup(props: any) {
+    const route = useRoute();
+    const store = useStore();
+    
+    const id = route.params.rockstarId;
+    const rockstar = computed( (): RockstarShape => {
+      return store.getters['rockstars/getRockstar'];
+    });
+    
+    onMounted(() => {
+      store.dispatch('rockstars/getRockstar', route.params.rockstarId);
+      document.title = rockstar.value.name;
+    });
+    
+    // !! opzoeken: Composition API
+    return {
+      rockstar
+    }
+  },
+};
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+@import "@/styles/variables.scss";
 
 </style>
