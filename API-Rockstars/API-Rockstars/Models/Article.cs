ae2a3eb7d81f76709d5c1a3ac851b0222b65d174
Muijsenberg,Reno R.F <r.muijsenberg@student.fontys.nl>
2022-04-07 14:11:21 +0200

    Add thumbnail to article

--- API-Rockstars/API-Rockstars/Models/Article.cs
+++ API-Rockstars/API-Rockstars/Models/Article.cs
@@ -10,6 +10,7 @@ public class Article
     [Key] public Guid Id { get; set; }
     [Required] public Guid TribeId { get; set; }
     [Required] public Guid RockstarId { get; set; }
+    public string? Thumbnail { get; set; }
     [Required] public string Title { get; set; }
     [Required] public string Content { get; set; }
     [NotMapped] public string? TribeName { get; set; }
