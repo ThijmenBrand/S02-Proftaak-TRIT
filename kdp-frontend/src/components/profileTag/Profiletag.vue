4ff03398e4792a11a076c49af5288a32bdad6d9c
code-syl <ejjvandelaar@gmail.com>
2022-04-08 21:26:45 +0200

    Added alt text + translations to the profile pictures above articles

--- kdp-frontend/src/components/profileTag/Profiletag.vue
+++ kdp-frontend/src/components/profileTag/Profiletag.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="profile-tag-container">
     <div class="profile-image-container">
-      <img class="profile-image" :src="`data:image/png;base64,${image}`" />
+      <img
+          class="profile-image"
+          :src="`data:image/png;base64,${image}`"
+          :alt="$t('rockstar-page.profile-picture-alt-text', { name: this.name })"
+      />
     </div>
     <p class="rockstar">{{ name }}</p>
     <p class="rockstar-role">{{ role }}</p>
