75e163f282ecb9f5829a0749516519a1155e7a79
code-syl <ejjvandelaar@gmail.com>
2022-04-08 21:54:55 +0200

    Moved LocaleSelector to its own component and stylesheet

--- kdp-frontend/src/components/navigation/BaseNavigation.vue
+++ kdp-frontend/src/components/navigation/BaseNavigation.vue
@@ -43,31 +43,7 @@
           </a>
         </div>
 
-        <div class="locale-change-container">
-          <div class="locale-container-title"><i>{{ $t('menu.change-language-prompt') }}</i></div>
-          <div class="locale-container">
-            <!--<select v-model="$i18n.locale">
-              <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">{{ locale }}</option>
-            </select>-->
-            <!-- The commented snippet is for a simple dropdown-->
-            <div
-                class="locale-single"
-                v-for="locale in $i18n.availableLocales.reverse()"
-                :key="`locale-${locale}`">
-              <input
-                  type="radio"
-                  :id="locale"
-                  v-model="$i18n.locale"
-                  :value="locale">
-              <label :for="locale">
-                <img
-                    :src="require('@/assets/flags/' + locale + '.svg')"
-                    :alt="$t('menu.change-language-prompt.flags.generic')"
-                />
-              </label>
-            </div>
-          </div>
-        </div>
+        <LocaleSelector />
       </div>
 
     </div>
@@ -76,8 +52,10 @@
 
 <script lang="ts">
 import { ref } from "vue";
+import LocaleSelector from "@/components/localeSelector/LocaleSelector.vue";
 
 export default {
+  components: { LocaleSelector },
   setup() {
     const isOpened = ref(false);
 
