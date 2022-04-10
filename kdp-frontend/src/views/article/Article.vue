8b9f4804e1273b22876a805284910df751230e32
Brand, Thijmen T.A <thijmen.brand@student.fontys.nl>
2022-04-10 18:22:31 +0200

    made a start

--- kdp-frontend/src/views/article/Article.vue
+++ kdp-frontend/src/views/article/Article.vue
@@ -12,9 +12,37 @@
       ></ProfileTag>
     </div>
   </div>
-  <div class="background-container">
-    <div class="content-container article-content">
-      <Blog :articleContent="articleDetails.content" />
+  <div class="background-container row">
+    <div class="content-container">
+      <div class="article-info">
+        <div class="left-article-info">
+          <span class="info-item"
+            ><View class="icon" /> {{ mockData.views }}</span
+          >
+          <span class="info-item"
+            ><Star v-if="!mockData.liked" class="icon" /><StarFilled
+              v-else
+              class="icon"
+            />
+            {{ mockData.likes }}</span
+          >
+          <span class="info-item"
+            ><ChatLineSquare class="icon" /> {{ mockData.comments }}</span
+          >
+        </div>
+        <div class="right-article-info">
+          <span class="info-item">
+            <CirclePlus v-if="!mockData.saved" class="icon" /><CirclePlusFilled
+              v-else
+              class="icon"
+            />
+          </span>
+        </div>
+      </div>
+      <div class="article-content column left">
+        <Blog :articleContent="articleDetails.content" />
+      </div>
+      <div class="column right"></div>
     </div>
   </div>
 </template>
@@ -30,6 +58,14 @@ import { RockstarShape } from "@/models/Rockstar";
 import Blog from "./components/Blog.vue";
 import Loader from "@/components/loader/Loader.vue";
 import ProfileTag from "@/components/profileTag/Profiletag.vue";
+import {
+  Star,
+  View,
+  StarFilled,
+  ChatLineSquare,
+  CirclePlusFilled,
+  CirclePlus,
+} from "@element-plus/icons-vue";
 
 export default {
   name: "Article-view",
@@ -37,6 +73,12 @@ export default {
     Blog,
     ProfileTag,
     Loader,
+    View,
+    Star,
+    StarFilled,
+    ChatLineSquare,
+    CirclePlusFilled,
+    CirclePlus,
   },
   setup() {
     const route = useRoute();
@@ -59,11 +101,19 @@ export default {
       (): ArticleShape => store.getters["article/getArticle"]
     );
 
+    const mockData = {
+      views: 10,
+      likes: 4,
+      liked: false,
+      comments: 1,
+      saved: false,
+    };
+
     const getRockstar = computed((): RockstarShape => {
       return store.getters["article/getRockstar"];
     });
 
-    return { articleId, articleDetails, loading, getRockstar };
+    return { articleId, articleDetails, loading, getRockstar, mockData };
   },
 };
 </script>
