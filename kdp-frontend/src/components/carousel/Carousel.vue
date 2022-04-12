3002e940accf558caf1ffdacec30dc577c4c95e7
Brand, Thijmen T.A <thijmen.brand@student.fontys.nl>
2022-04-12 13:00:11 +0200

    foramt

--- kdp-frontend/src/components/carousel/Carousel.vue
+++ kdp-frontend/src/components/carousel/Carousel.vue
@@ -1,25 +1,31 @@
 <template>
   <carousel :items-to-show="1" class="carousel-wrapper">
     <slide v-for="(item, index) in spotifyLinks" :key="index" class="carousel">
-      <iframe :src="item.spotifyLink + '&theme=black'" scrolling="no" class="spotify-iframe" height="232" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture;"></iframe>
+      <iframe
+        :src="item.spotifyLink + '&theme=black'"
+        scrolling="no"
+        class="spotify-iframe"
+        height="232"
+        frameBorder="0"
+        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture;"
+      ></iframe>
     </slide>
     <template #addons>
       <navigation />
-      <pagination/>
+      <pagination />
     </template>
   </carousel>
 </template>
 
 <script>
 // If you are using PurgeCSS, make sure to whitelist the carousel CSS classes
-import {Carousel, Slide, Pagination, Navigation} from 'vue3-carousel';
-import {onMounted} from "vue";
-import "@/styles/componentStyles/carousel/SpotifyCarousel.scss"
-import 'vue3-carousel/dist/carousel.css';
-
+import { Carousel, Slide, Pagination, Navigation } from "vue3-carousel";
+import { onMounted } from "vue";
+import "@/styles/componentStyles/carousel/SpotifyCarousel.scss";
+import "vue3-carousel/dist/carousel.css";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Carousel,
     Slide,
@@ -32,7 +38,7 @@ export default {
       required: true,
       default() {
         return [];
-      }
+      },
     },
   },
 };
