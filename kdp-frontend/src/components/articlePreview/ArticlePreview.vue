350c50878e56342fe75faafb42db8d0f2b461210
LukasJansen100 <93530655+LukasJansen100@users.noreply.github.com>
2022-04-14 10:47:20 +0200

    Added filter for html tags

--- kdp-frontend/src/components/articlePreview/ArticlePreview.vue
+++ kdp-frontend/src/components/articlePreview/ArticlePreview.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <div>
+  <div class="article-container">
+    <div class="image-container">
       <img
         src="@/assets/images/article-placeholder-image.jpg"
         class="preview article-image"
@@ -10,10 +10,10 @@
           })
         "
       />
-      <div>
+      <div class="article-content-container">
         <h4 class="article-preview-title">{{ name }}</h4>
         <p class="content">
-          {{ limitContent(content) }}
+          {{limitContent(cleanText(content))}}
         </p>
         <p class="rockstar-name">
           {{ rockstarName }}
@@ -40,7 +40,11 @@ export default {
       return content.length < 250 ? content : content.substring(0, 250) + "...";
     };
 
-    return { limitContent };
+    const cleanText = (content: string): string => {
+      return content.replace(/<\/?[^>]+(>|$)/g, "");
+    }
+
+    return { limitContent, cleanText };
   },
 };
 </script>
