32320ae0348c8684ab3e3c77e5358dbe7ba9a8c1
brettmulder2002@icloud.com <Brettje2209>
2022-04-14 13:52:04 +0200

    Fixed small changes for set localstorage

--- kdp-frontend/src/config/SetLocalstorage.ts
+++ kdp-frontend/src/config/SetLocalstorage.ts
@@ -1,17 +1,15 @@
 export default function SetLocalStorage() {
-  const UUIDV4 = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
-    /[xy]/g,
-    function(c) {
-      const r = (Math.random() * 16) | 0,
-        v = c == "x" ? r : (r & 0x3) | 0x8;
-      return v.toString(16);
-    }
-  );
-
-  if (!localStorage.getItem("UUIDV4")) {
-    localStorage.setItem("UUIDV4", UUIDV4);
+  if (!localStorage.getItem("UserId-uuid")) {
+    const UUIDV4 = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+      /[xy]/g,
+      function(c) {
+        const r = (Math.random() * 16) | 0,
+          v = c == "x" ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+      }
+    );
+    localStorage.setItem("UserId-uuid", UUIDV4);
   } else {
     return;
   }
-  return UUIDV4;
 }
