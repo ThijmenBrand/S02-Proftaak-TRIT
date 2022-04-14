e84e053b9469920ef9fce7a735bf19e61ecb2b6a
antonyle63801 <93910526+antonyle63801@users.noreply.github.com>
2022-04-14 11:34:41 +0200

    added searchbar and filter to tribe page

--- kdp-frontend/src/views/tribes/tribePage/TribePage.vue
+++ kdp-frontend/src/views/tribes/tribePage/TribePage.vue
@@ -14,6 +14,32 @@
         />
       </div>
     </div>
+    <div class="search-bar">
+      <input
+        v-model="searchQuery"
+        :placeholder="$t('explore-articles-page.search-bar.placeholder')"
+        class="search-input"
+      />
+      <div class="custom-select">
+        <select class="select" v-model="selectedFilter">
+         <option class="select-item" value="">
+           {{ $t("explore-articles-page.search-bar.select-filter") }}
+         </option>
+         <option class="select-item" value="new">
+           {{ $t("explore-articles-page.search-bar.sort-newest") }}
+         </option>
+        <option class="select-item" value="old">
+          {{ $t("explore-articles-page.search-bar.sort-oldest") }}
+        </option>
+        <option class="select-item" value="a-z">
+           {{ $t("explore-articles-page.search-bar.sort-title-az") }}
+        </option>
+        <option class="select-item" value="z-a">
+          {{ $t("explore-articles-page.search-bar.sort-title-za") }}
+        </option>
+       </select>
+      </div>
+    </div>
     <div class="background-container">
       <div class="content-container">
         <h3 class="articles-overview-title">
@@ -22,19 +48,22 @@
         <div class="loader-container" v-if="loading">
           <Loader />
         </div>
-        <div class="articles-container">
+        <div class="articles-container" v-else>
           <router-link
-            :to="{ name: 'article', params: { articleId: article.id } }"
-            v-for="(article, index) in tribeArticles"
+            v-for="(article, index) in filteredArticles"
             :key="index"
+            :to="{
+              name: 'article',
+              params: { articleId: article.id },
+            }"
             class="article"
           >
-            <article-preview
-              :name="article.title"
-              :content="article.content"
-              :rockstarName="article.rockstarName"
-              :articlePublishDate="article.publishDate"
-            />
+          <article-preview
+            :name="article.title"
+            :content="article.content"
+            :rockstarName="article.rockstarName"
+            :articlePublishDate="article.publishDate"
+          />
           </router-link>
         </div>
         <h3 class="podcasts-overview-title">Podcasts</h3>
@@ -45,7 +74,7 @@
 </template>
 
 <script lang="ts">
-import { computed, onMounted } from "vue";
+import { computed, onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
 import { useStore } from "vuex";
 
@@ -68,6 +97,9 @@ export default {
   setup() {
     const route = useRoute();
     const store = useStore();
+
+    const searchQuery = ref("");
+    const selectedFilter = ref("");
 
     const loading = computed(() => store.getters["isLoading"]);
 
@@ -115,8 +147,80 @@ export default {
       return spotify;
     });
 
+    const filteredArticles = computed((): ArticleShape[] => {
+      let returnArray: ArticleShape[] = [];
+      articles.value.forEach((article) => {
+        if (article.rockstarName != null || article.tribeName != null) {
+          if (
+            article.title
+              .toLowerCase()
+              .indexOf(searchQuery.value.toLowerCase()) > -1 ||
+            article.rockstarName
+              .toLowerCase()
+              .indexOf(searchQuery.value.toLowerCase()) > -1
+          ) {
+            returnArray.push(article);
+          }
+        }
+      });
+      if (selectedFilter.value == "a-z") {
+        returnArray = returnArray.sort(
+          (firstComparisonObject, seccondComparisonObject) => {
+            let firstObejctToLower = firstComparisonObject.title.toLowerCase(),
+              secondObjectToLower = seccondComparisonObject.title.toLowerCase();
+            if (firstObejctToLower < secondObjectToLower) {
+              return -1;
+            }
+            if (firstObejctToLower > secondObjectToLower) {
+              return 1;
+            }
+            return 0;
+          }
+        );
+      }
+      if (selectedFilter.value == "z-a") {
+        returnArray = returnArray.sort(
+          (firstComparisonObject, seccondComparisonObject) => {
+            let firstObjectToLower = firstComparisonObject.title.toLowerCase(),
+              secondObjectToLower = seccondComparisonObject.title.toLowerCase();
+            if (firstObjectToLower < secondObjectToLower) {
+              return 1;
+            }
+            if (firstObjectToLower > secondObjectToLower) {
+              return -1;
+            }
+            return 0;
+          }
+        );
+      }
+      if (selectedFilter.value == "new") {
+        returnArray = returnArray.sort(
+          (firstComparisonObject, seccondComparisonObject) => {
+            return (
+              new Date(firstComparisonObject.publishDate.toString()).valueOf() -
+              new Date(seccondComparisonObject.publishDate.toString()).valueOf()
+            );
+          }
+        );
+      }
+      if (selectedFilter.value == "old") {
+        returnArray = returnArray.sort(
+          (firstComparisonObject, seccondComparisonObject) => {
+            return (
+              new Date(firstComparisonObject.publishDate.toString()).valueOf() -
+              new Date(seccondComparisonObject.publishDate.toString()).valueOf()
+            );
+          }
+        );
+      }
+      return returnArray;
+    });
+    
     return {
       tribeArticles,
+      filteredArticles, 
+      searchQuery, 
+      selectedFilter,
       articles,
       rockstars,
       currentTribe,
