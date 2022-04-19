e5123d346378c535d2e4cb0898de0ede54493fa4
Brand, Thijmen T.A <thijmen.brand@student.fontys.nl>
2022-04-19 13:24:58 +0200

    fixed bug

--- kdp-frontend/src/services/customDateTime.ts
+++ kdp-frontend/src/services/customDateTime.ts
@@ -1,6 +1,6 @@
 export default function getCustomDateTime(date: string): string {
   let custom = date;
-  if (date != "") {
+  if (date) {
     const language = navigator.language;
     custom = new Date(date.toString()).toLocaleDateString(language);
     return custom;
