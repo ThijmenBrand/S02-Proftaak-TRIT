7559b05edc6ce1eab5cc1b2f8da31bd10337ba83
RenoMuijsenberg <43666923+RenoMuijsenberg@users.noreply.github.com>
2022-04-19 13:08:28 +0200

    Update authConfig.ts

--- kdp-frontend/src/config/authConfig.ts
+++ kdp-frontend/src/config/authConfig.ts
@@ -4,7 +4,7 @@ export const msalConfig = {
   auth: {
     clientId: "d0b1fd93-02ac-44ce-94cc-6ed26b112ce1",
     authority: "https://login.microsoftonline.com/common",
-    redirectUri: "http://localhost:8080/",
+    redirectUri: "https://kind-coast-0cd0e9110.1.azurestaticapps.net",
   },
   cache: {
     cacheLocation: "localStorage",
