bd544a65340a437585873393b4a9abead11becf5
code-syl <ejjvandelaar@gmail.com>
2022-04-26 11:18:55 +0200

    Added div for OnDemand request

--- kdp-frontend/src/components/rockstarView/RockstarView.vue
+++ kdp-frontend/src/components/rockstarView/RockstarView.vue
@@ -3,13 +3,13 @@
     <div class="rockstar-card">
       <div class="col-image">
         <img
-          class="rockstar-profile-image"
-          :src="rockstar.image"
-          :alt="
+            :alt="
             $t('rockstar-page.profile-picture-alt-text', {
               name: rockstar.name,
             })
           "
+            :src="rockstar.image"
+            class="rockstar-profile-image"
         />
       </div>
       <div class="col-information">
@@ -19,25 +19,25 @@
         <div class="row-description">
           <a class="DIN2014-Regular">{{ rockstar.description }}</a>
         </div>
-
         <div class="col-socials">
-
-          <a  v-if="rockstar.twitter != null" class="social-component" :href="rockstar.twitter">
-            <img class="social-image" src="@/assets/images/socials/twitter.svg" alt="Twitter"/>
+          <a v-if="rockstar.twitter != null" :href="rockstar.twitter" class="social-component">
+            <img alt="Twitter" class="social-image" src="@/assets/images/socials/twitter.svg"/>
           </a>
-          
-          <a v-if="rockstar.linkedIn != null" class="social-component" :href="rockstar.linkedIn">
-            <img class="social-image" src="@/assets/images/socials/linkedin.svg" alt="LinkedIn"/>
+          <a v-if="rockstar.linkedIn != null" :href="rockstar.linkedIn" class="social-component">
+            <img alt="LinkedIn" class="social-image" src="@/assets/images/socials/linkedin.svg"/>
           </a>
-
-          <a v-if="rockstar.email != null" class="social-component" :href="'mailto:' + rockstar.email">
-            <img class="social-image" src="@/assets/images/socials/email.svg" :alt="rockstar.email"/>
+          <a v-if="rockstar.email != null" :href="'mailto:' + rockstar.email" class="social-component">
+            <img :alt="rockstar.email" class="social-image" src="@/assets/images/socials/email.svg"/>
           </a>
-
-          <a v-if="rockstar.phone != null" class="social-component" :href="'tel:' + rockstar.phone">
-            <img class="social-image" src="@/assets/images/socials/phone.svg" :alt="rockstar.phone"/>
+          <a v-if="rockstar.phone != null" :href="'tel:' + rockstar.phone" class="social-component">
+            <img :alt="rockstar.phone" class="social-image" src="@/assets/images/socials/phone.svg"/>
           </a>
-
+        </div>
+        <div class="row-ondemand">
+          <div class="col-ondemand-text">OnDemand aanvraag</div>
+          <div class="col-ondemand-icon">
+            <img src="@/assets/images/icon-fastforward.svg">
+          </div>
         </div>
       </div>
     </div>
@@ -45,7 +45,7 @@
 </template>
 
 <script lang="ts">
-import { RockstarShape } from "@/models/Rockstar";
+import {RockstarShape} from "@/models/Rockstar";
 
 export default {
   name: "RockstarView",
@@ -56,7 +56,7 @@ export default {
 </script>
 
 <style
-  scoped
-  lang="scss"
-  src="@/styles/pageStyles/RockstarInformationPage/RockstarView.scss"
+    lang="scss"
+    scoped
+    src="@/styles/pageStyles/RockstarInformationPage/RockstarView.scss"
 />
