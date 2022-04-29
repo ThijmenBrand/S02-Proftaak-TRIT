6079b08b0a79e66c93db09684a0f291dab59de7f
TeunMos <teun.mos@gmail.com>
2022-04-29 11:09:17 +0200

    added pagination to rockstar page

--- kdp-frontend/src/services/callFunctions/rockstar.ts
+++ kdp-frontend/src/services/callFunctions/rockstar.ts
@@ -5,8 +5,11 @@ const rockstarService = {
   getRockstar(rockstarId: string): Promise<AxiosResponse<any, any>> {
     return API.get(`/rockstar/${rockstarId}`);
   },
-  getArticles(rockstarId: string): Promise<AxiosResponse<any, any>> {
-    return API.get(`/article/GetArticlesByRockstar/${rockstarId}/start/0/limit/100`);
+  getArticles(rockstarId: string, start: number, limit: number): Promise<AxiosResponse<any, any>> {
+    return API.get(`/article/GetArticlesByRockstar/${rockstarId}/start/${start}/limit/${limit}`);
+  },
+  getArticleCount(rockstarId: string): Promise<AxiosResponse<any, any>> {
+    return API.get(`article/count/rockstar/${rockstarId}`);
   },
 };
 
