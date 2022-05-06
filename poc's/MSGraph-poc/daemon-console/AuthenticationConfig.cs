1ac224b60b3e593f70329f3b1af3b1244a6d5079
Brand, Thijmen T.A <thijmen.brand@student.fontys.nl>
2022-05-06 12:36:49 +0200

    added-graph-example

---
+++ poc's/MSGraph-poc/daemon-console/AuthenticationConfig.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Globalization;
+using System.IO;
+
+namespace daemon_console
+{
+    public class AuthenticationConfig
+    {
+        public string Instance { get; set; } = "https://login.microsoftonline.com/{0}";
+        public string ApiUrl { get; set; } = "https://graph.microsoft.com/";
+        public string Tenant { get; set; }
+        public string ClientId { get; set; }
+        public string Authority
+        {
+            get
+            {
+                return String.Format(CultureInfo.InvariantCulture, Instance, Tenant);
+            }
+        }
+        public string ClientSecret { get; set; }
+        public string CertificateName { get; set; }
+        public static AuthenticationConfig ReadFromJsonFile(string path)
+        {
+            IConfigurationRoot Configuration;
+
+            var builder = new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile(path);
+
+            Configuration = builder.Build();
+            return Configuration.Get<AuthenticationConfig>();
+        }
+    }
+
+
+
+}
+
---
+++ poc's/MSGraph-poc/daemon-console/ProtectedApiCallHelper.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace daemon_console
+{
+    public class ProtectedApiCallHelper
+    {
+        public ProtectedApiCallHelper(HttpClient httpClient)
+        {
+            HttpClient = httpClient;
+        }
+
+        protected HttpClient HttpClient { get; private set; }
+
+        public async Task CallWebApiAndProcessResultASync(string webApiUrl, string accessToken, Action<JObject> processResult)
+        {
+            if (!string.IsNullOrEmpty(accessToken))
+            {
+                var defaultRequestHeaders = HttpClient.DefaultRequestHeaders;
+                if (defaultRequestHeaders.Accept == null || !defaultRequestHeaders.Accept.Any(m => m.MediaType == "application/json"))
+                {
+                    HttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                }
+                defaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+                HttpResponseMessage response = await HttpClient.GetAsync(webApiUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                    JObject result = JsonConvert.DeserializeObject(json) as JObject;
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    processResult(result);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Failed to call the web API: {response.StatusCode}");
+                    string content = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine($"Content: {content}");
+                }
+                Console.ResetColor();
+            }
+        }
+    }
+}
