c4a6d4fb5808cd267a99cdf4a46da27db1e98451
LukasJansen100 <93530655+LukasJansen100@users.noreply.github.com>
2022-05-12 15:18:36 +0200

    fixed conflict

--- kdp-frontend/src/views/Home.vue
+++ kdp-frontend/src/views/Home.vue
@@ -3,7 +3,6 @@
     <div class="flex-container">
       <div class="left-flexbox">
         <div class="interesting-tribes-container">
-<<<<<<< HEAD
           <Loader v-if="loading" />
           <h3 v-else v-for="(tribe, index) in tribesList" :key="index">
             <router-link
@@ -19,12 +18,6 @@
         </div>
         <div class="about-container">
           <h1>{{$t("about-us")}}</h1>
-=======
-          <p>hmm ja hele interesante tribes</p>
-        </div>
-        <div class="about-container">
-          <h1>About us</h1>
->>>>>>> e137f7e99dad98d02e4f3ed16ed84231c2d6c409
           <p>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam
             aperiam cumque repudiandae qui libero ab accusamus deleniti nulla ad
@@ -36,7 +29,6 @@
         </div>
 
         <div class="highlighted-articles">
-<<<<<<< HEAD
           <h1>
             {{ $t("highlighted-articles") }}
           </h1>
@@ -69,42 +61,11 @@
               {{ $t("article.article-error") }}
             </p>
           </div>
-=======
-          <h1>Uitgelichte artikelen</h1>
-          <div class="content-container">
-      <div v-if="loading">
-        <Loader />
-      </div>
-      <div
-        class="articles-container"
-        v-else-if="!loading && articles.length > 0"
-      >
-        <router-link
-          v-for="(article, index) in articles"
-          :key="index"
-          :to="{
-            name: 'article',
-            params: { articleId: article.id },
-          }"
-          class="article"
-        >
-          <article-preview
-            :name="article.title"
-            :content="article.content"
-            :rockstarName="article.rockstarName"
-            :articlePublishDate="article.publishDate"
-          />
-        </router-link>
-      </div>
-      <p class="article-error" v-else>{{ $t("article.article-error") }}</p>
-    </div>
->>>>>>> e137f7e99dad98d02e4f3ed16ed84231c2d6c409
         </div>
       </div>
 
       <div class="right-flexbox">
         <div class="highlighted-rockstars">
-<<<<<<< HEAD
           <p>hier komen highlighted rockstars</p>
           <profiletag
           v-for="(rockstar, index) in rockstars"
@@ -117,12 +78,6 @@
         />
         </div>
       </div>
-=======
-        <p>hier komen highlighted rockstars</p>
-      </div>
-      </div>
-      
->>>>>>> e137f7e99dad98d02e4f3ed16ed84231c2d6c409
     </div>
   </div>
 </template>
@@ -133,21 +88,15 @@ import { useStore } from "vuex";
 import Loader from "@/components/loader/Loader.vue";
 import ArticleShape from "@/models/Article";
 import ArticlePreview from "@/components/articlePreview/ArticlePreview.vue";
-<<<<<<< HEAD
 import { TribeShape } from '@/models/Tribe';
 import { RockstarShape } from '@/models/Rockstar';
 import profiletag from "@/components/profileTag/Profiletag.vue"
-=======
->>>>>>> e137f7e99dad98d02e4f3ed16ed84231c2d6c409
 
 export default {
   components: {
     ArticlePreview,
     Loader,
-<<<<<<< HEAD
     profiletag,
-=======
->>>>>>> e137f7e99dad98d02e4f3ed16ed84231c2d6c409
   },
 
   setup() {
@@ -155,21 +104,16 @@ export default {
     const loading = computed(() => store.getters["isLoading"]);
 
     onMounted(async () => {
-<<<<<<< HEAD
       store.commit("SET_CURRENT_PAGE", 1);
       await store.dispatch("getAllArticles", 6);
       await store.dispatch("getAllRockstars");
       await store.dispatch("tribes/getAllTribes");
-=======
-      await store.dispatch("getAllArticles");
->>>>>>> e137f7e99dad98d02e4f3ed16ed84231c2d6c409
     });
 
     const articles = computed((): ArticleShape[] => {
       return store.getters["getAllArticles"];
     });
 
-<<<<<<< HEAD
     
     
     const rockstars = computed((): RockstarShape[] => {
@@ -186,10 +130,6 @@ export default {
 
     console.log(rockstars);
     return { store, articles, loading, tribesList, rockstars };
-=======
-
-    return { store, articles, loading};
->>>>>>> e137f7e99dad98d02e4f3ed16ed84231c2d6c409
   },
 };
 </script>
