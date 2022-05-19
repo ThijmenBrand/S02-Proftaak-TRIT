7c4a8a09a9d200d676752aa7cc7d632e2b0ca3e0
brettmulder2002@icloud.com <Brettje2209>
2022-05-19 10:14:41 +0200

    Changed API URl to new url

--- kdp-frontend/src/services/callFunctions/explore.ts
+++ kdp-frontend/src/services/callFunctions/explore.ts
@@ -13,7 +13,7 @@ const exploreService = {
     limit: number,
     searchData: string
   ): Promise<AxiosResponse<any, any>> {
-    return API.get(`/article/new/start/${start}/limit/${limit}`, {
+    return API.get(`/article/start/${start}/limit/${limit}`, {
       params: { data: searchData },
     });
   },
