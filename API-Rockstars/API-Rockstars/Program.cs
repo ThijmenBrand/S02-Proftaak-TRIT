00590479a655fb483f3abe12eb88b2a47742bcf6
Brand, Thijmen T.A <thijmen.brand@student.fontys.nl>
2022-06-16 11:13:12 +0200

    add cors

--- API-Rockstars/API-Rockstars/Program.cs
+++ API-Rockstars/API-Rockstars/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Hosting;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
